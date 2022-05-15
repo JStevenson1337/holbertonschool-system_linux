e3db306e2b28a57e4901e5fc351c2d673ad6bfab
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-05-15 18:15:05 -0500

    corrected for betty

--- 0x01-ls/main.c
+++ 0x01-ls/main.c
@@ -1,30 +1,28 @@
 #include "hls.h"
-
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
 
-int main(int argc, char * argv[])
+
+int main(int argc, char *argv[])
 {
-    DIR *dir;
-    struct dirent *ent;
-    char *path = ".";
+	DIR *dir;
+	struct dirent *ent;
+	char *path = ".";
 
-
-    if (argc > 1)
-        path = argv[1];
-    
-    dir = opendir(path);
-    if (dir == NULL)
-        return 1;
-    
-    while ((ent = readdir(dir)) != NULL)
-    {
-        if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
-            continue;
-        printf(" %s ", ent->d_name);
-    }
-
-    closedir(dir);
-    return 0;
+	if (argc > 1)
+		path = argv[1];
+	dir = opendir(path);
+	if (dir == NULL)
+		return (1);
+	while ((ent = readdir(dir)) != NULL)
+	{
+		if (ent->d_name[0] == '.')
+			continue;
+		{
+			printf(" %s ", ent->d_name);
+		}
+	}
+	closedir(dir);
+	return (0);
 }
