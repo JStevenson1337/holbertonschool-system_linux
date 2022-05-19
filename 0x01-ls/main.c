e3ce7a9bbd38e5746d219b31f7a7b840f354cfd1
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-05-19 15:00:12 -0500

    betty corrections

--- 0x01-ls/main.c
+++ 0x01-ls/main.c
@@ -1,61 +1,82 @@
 #include "hls.h"
 
-void hls(const char *dir,int op_a,int op_1)
+
+/**
+ * hls - Converts a directory to a list of files
+ * @dir: The directory to convert
+ * @op_a: option a
+ * @op_1: option 1
+*/
+void hls(const char *dir, int op_a, int op_1)
 {
-	//Here we will list the directory
 	struct dirent *d;
 	DIR *dh = opendir(dir);
+
 	if (!dh)
 	{
 		if (errno == ENOENT)
 		{
-			//If the directory is not found
 			perror("Directory doesn't exist");
 		}
 		else
 		{
-			//If the directory is not readable then throw error and exit
 			perror("Unable to read directory");
 		}
 		exit(EXIT_FAILURE);
 	}
-	//While the next entry is not readable we will print directory files
 	while ((d = readdir(dh)) != NULL)
 	{
-		//If hidden files are found we continue
 		if (!op_a && d->d_name[0] == '.')
 			continue;
 		printf("%s  ", d->d_name);
-		if(op_1) printf("\n");
+		if (op_1)
+		{
+			printf("\n");
+		}
 	}
-	if(!op_1)
+	if (!op_1)
 	printf("\n");
 }
+
+/**
+ * main - main program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: List of nodes
+*/
 int main(int argc, const char *argv[])
 {
+
 	if (argc == 1)
 	{
-		hls(".",0,0);
+		hls(".", 0, 0);
 	}
 	else if (argc == 2)
 	{
 		if (argv[1][0] == '-')
 		{
-			//Checking if option is passed
-			//Options supporting: a, l
 			int op_a = 0, op_1 = 0;
-			char *p = (char*)(argv[1] + 1);
-			while(*p){
-				if(*p == 'a') op_a = 1;
-				else if(*p == '1') op_1 = 1;
-				else{
+			char *p = (char *)(argv[1] + 1);
+
+			while (*p)
+			{
+				if (*p == 'a')
+				{
+					op_a = 1;
+				}
+				else if (*p == '1')
+				{
+					op_1 = 1;
+				}
+				else
+				{
 					perror("Option not available");
 					exit(EXIT_FAILURE);
 				}
 				p++;
 			}
-			hls(".",op_a,op_1);
+			hls(".", op_a, op_1);
 		}
 	}
-	return 0;
+	return (0);
 }
