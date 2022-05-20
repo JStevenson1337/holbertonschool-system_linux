d5d6851aa4e4f9cfc8bc26fdde34bc2f7fbeb75c
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-05-20 01:40:40 -0500

    trying more things

--- 0x01-ls/hls.h
+++ 0x01-ls/hls.h
@@ -14,9 +14,33 @@
 #define current_directory "."
 #define parent_directory ".."
 
+
+/**
+ * struct file_s - singly linked list
+ * @str: variable
+ * @size: variable
+ * @time: variable
+ * @next: points to the next node
+ * @prev: points to the prev node
+ * @var: environment variables.
+ * Description: singly linked list node to store the env
+ */
+typedef struct file_s
+{
+	char *var;
+	long int size;
+	unsigned long int time;
+	struct file_s *prev;
+	struct file_s *next;
+} file_s;
+
+char *validate_file_s(const char *dirname, const char *filename);
+char *validate_args(char **argv, char **errors);
+char **validate_dir(int argc, char **argv, int *ret, int *fcount, int *ecount, int *ficount, char *args);
+void error_handler(int err, const char *dirname, const char *progname);
 void listFiles(const char *dirname);
 void error_handler(int error, const char *path, const char *filename);
-
+int _strcmp(char *s1, char *s2);
 
 void print_list_normalized(const char *dirname);
 void print_list_1_by_1(const char *dirname);
@@ -24,7 +48,7 @@ void print_list_rev(const char *dirname);
 void print_list_time(const char *dirname);
 void print_list_size(const char *dirname);
 
+struct stat get_stat(const char *path);
 
-int set_flags(const char **flag);
 
 #endif /*_HLS_H*/
--- 0x01-ls/main.c
+++ 0x01-ls/main.c
@@ -23,11 +23,11 @@ void listFiles(const char* dirname)
 	{
 		if (entry->d_name[0] != '.')
 		{
-			print_list_normalized(entry->d_name);
+			printf("%s  ", entry->d_name);
 		}
 	}
+	printf("\n");
 	closedir(dir);
-
 }
 /**
  * main - main function
@@ -35,105 +35,20 @@ void listFiles(const char* dirname)
  * @argv: argv Array of arguments
  * @return 0 if success, -1 otherwise
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i;
-	int **flag = 0;
-
-	if (argc < 2)
+	if (!argc || argv == NULL)
 	{
-		listFiles(current_directory);
-		return (0);
-	}
-
-	flag = malloc(sizeof(int *) * argc);
-	if (!flag)
-	{
-		error_handler(-1, "", "hls");
 		return (-1);
 	}
-
-	for (i = 0; i < argc; i++)
+	if (argc == 1)
 	{
-		flag[i] = malloc(sizeof(int) * 2);
-		if (!flag[i])
-		{
-			error_handler(-1, "", "hls");
-			return (-1);
-		}
-		flag[i][0] = 0;
-		flag[i][1] = 0;
+		listFiles(".");
 	}
-
-	for (i = 1; i < argc; i++)
+	else
 	{
-		if (argv[i][0] == '-')
-		{
-			if (argv[i][1] == 'R')
-			{
-				flag[i][0] = 1;
-				flag[i][1] = 1;
-			}
-			else if (argv[i][1] == 'a')
-			{
-				flag[i][0] = 1;
-				flag[i][1] = 2;
-			}
-			else if (argv[i][1] == 't')
-			{
-				flag[i][0] = 1;
-				flag[i][1] = 3;
-			}
-			else if (argv[i][1] == 'S')
-			{
-				flag[i][0] = 1;
-				flag[i][1] = 4;
-			}
-			else if (argv[i][1] == '1')
-			{
-				flag[i][0] = 1;
-				flag[i][1] = 5;
-			}
-			else
-			{
-				error_handler(-1, "", "hls");
-				return (-1);
-			}
-		}
-		else
-		{
-			listFiles(argv[i]);
-		}
+		listFiles(argv[1]);
 	}
-
-	for (i = 1; i < argc; i++)
-	{
-		if (flag[i][0] == 1)
-		{
-			switch (flag[i][1])
-			{
-			case 1:
-				print_list_1_by_1(argv[i]);
-				break;
-			case 2:
-				print_list_normalized(argv[i]);
-				break;
-			case 3:
-				print_list_time(argv[i]);
-				break;
-			case 4:
-				print_list_size(argv[i]);
-				break;
-			case 5:
-				print_list_1_by_1(argv[i]);
-				break;
-			default:
-				error_handler(-1, "", "hls");
-				return (-1);
-			}
-		}
-	}
-
 	return (0);
 }
 
