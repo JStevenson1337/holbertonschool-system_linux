653a244aa623539f973830bab0e73403b62ab568
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-05-19 20:48:08 -0500

    fix: options

--- 0x01-ls/hls.h
+++ 0x01-ls/hls.h
@@ -18,4 +18,13 @@ void listFiles(const char *dirname);
 void error_handler(int error, const char *path, const char *filename);
 int _strcmp(char *s1, char *s2);
 
+void print_list_normalized(const char *dirname);
+void print_list_1_by_1(const char *dirname);
+void print_list_rev(const char *dirname);
+void print_list_time(const char *dirname);
+void print_list_size(const char *dirname);
+
+
+int set_flags(const char **flag);
+
 #endif /*_HLS_H*/
--- 0x01-ls/main.c
+++ 0x01-ls/main.c
@@ -8,39 +8,27 @@
  *
  * Return: void
  */
-void listFiles(const char *dirname)
+void listFiles(const char* dirname)
 {
-	struct dirent *entry;
-	struct stat statbuf;
-	char path[1024] = {0};
-	DIR *dir;
+	DIR* dir = opendir(dirname);
+	struct dirent* entry;
 
-	dir = opendir(dirname);
-	if (dir == NULL)
+	if (!dir)
 	{
 		error_handler(errno, dirname, "hls");
 		return;
 	}
+
 	while ((entry = readdir(dir)) != NULL)
 	{
-		if (_strcmp(entry->d_name, current_directory) == 0 ||
-			_strcmp(entry->d_name, parent_directory) == 0)
-			continue;
-		sprintf(path, "%s/%s", dirname, entry->d_name);
-		if (lstat(path, &statbuf) == -1)
+		if (entry->d_name[0] != '.')
 		{
-			error_handler(errno, path, "hls");
-			continue;
-		}
-		if (S_ISDIR(statbuf.st_mode) || S_ISREG(statbuf.st_mode))
-		{
-			printf("%s  ", entry->d_name);
+			print_list_normalized(entry->d_name);
 		}
 	}
-	printf("\n");
 	closedir(dir);
-}
 
+}
 /**
  * main - main function
  * @argc: Number of arguments
@@ -50,15 +38,102 @@ void listFiles(const char *dirname)
 int main(int argc, char *argv[])
 {
 	int i;
+	int **flag = 0;
+
+	if (argc < 2)
+	{
+		listFiles(current_directory);
+		return (0);
+	}
+
+	flag = malloc(sizeof(int *) * argc);
+	if (!flag)
+	{
+		error_handler(-1, "", "hls");
+		return (-1);
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		flag[i] = malloc(sizeof(int) * 2);
+		if (!flag[i])
+		{
+			error_handler(-1, "", "hls");
+			return (-1);
+		}
+		flag[i][0] = 0;
+		flag[i][1] = 0;
+	}
 
 	for (i = 1; i < argc; i++)
 	{
-		listFiles(argv[i]);
+		if (argv[i][0] == '-')
+		{
+			if (argv[i][1] == 'R')
+			{
+				flag[i][0] = 1;
+				flag[i][1] = 1;
+			}
+			else if (argv[i][1] == 'a')
+			{
+				flag[i][0] = 1;
+				flag[i][1] = 2;
+			}
+			else if (argv[i][1] == 't')
+			{
+				flag[i][0] = 1;
+				flag[i][1] = 3;
+			}
+			else if (argv[i][1] == 'S')
+			{
+				flag[i][0] = 1;
+				flag[i][1] = 4;
+			}
+			else if (argv[i][1] == '1')
+			{
+				flag[i][0] = 1;
+				flag[i][1] = 5;
+			}
+			else
+			{
+				error_handler(-1, "", "hls");
+				return (-1);
+			}
+		}
+		else
+		{
+			listFiles(argv[i]);
+		}
 	}
-	if (argc == 1)
+
+	for (i = 1; i < argc; i++)
 	{
-		listFiles(".");
+		if (flag[i][0] == 1)
+		{
+			switch (flag[i][1])
+			{
+			case 1:
+				print_list_1_by_1(argv[i]);
+				break;
+			case 2:
+				print_list_normalized(argv[i]);
+				break;
+			case 3:
+				print_list_time(argv[i]);
+				break;
+			case 4:
+				print_list_size(argv[i]);
+				break;
+			case 5:
+				print_list_1_by_1(argv[i]);
+				break;
+			default:
+				error_handler(-1, "", "hls");
+				return (-1);
+			}
+		}
 	}
+
 	return (0);
 }
 
