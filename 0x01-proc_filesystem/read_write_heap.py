d6315004fb1d08196e9714a2dd82fd580fa05d3d
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-06-09 00:02:04 -0500

    pycodestyle corrections

--- 0x01-proc_filesystem/read_write_heap.py
+++ 0x01-proc_filesystem/read_write_heap.py
@@ -4,7 +4,9 @@
     where <pid> is a pid, <curr_pid> is a string to search for in the heap, and
     <new_pid> replaces curr_pid.
 """
-import sys, re
+import sys
+import re
+
 
 def read_write_heap(pid, curr_pid, new_pid):
     """ replaces a string in the heap memory of a running process"""
@@ -19,41 +21,42 @@ def read_write_heap(pid, curr_pid, new_pid):
     regex = r'\b([\da-zA-Z]+)-([\da-zA-Z]+).*?\[heap]'
     heap_ranges = re.findall(regex, address_map)
     with open('/proc/{}/mem'.format(pid), 'r+b', 0) as mem:
-            
-            while True:
-                try:
-                    start = int(heap_ranges[0][0], 16)
-                    end = int(heap_ranges[0][1], 16)
-                    mem.seek(start)
-                    buffer = mem.read(end - start)
-                    target_location = buffer.find(curr_pid)
-                    if target_location != -1:
-                        mem.seek(start + target_location)
-                        difference = max(len(curr_pid) - len(new_pid), 0)
-                        mem.write(new_pid + bytes(difference))
-                    else:
-                        heap_ranges.pop(0)
-                except IndexError:
-                    break
-            mem.close()
+
+        while True:
+            try:
+                start = int(heap_ranges[0][0], 16)
+                end = int(heap_ranges[0][1], 16)
+                mem.seek(start)
+                buffer = mem.read(end - start)
+                target_location = buffer.find(curr_pid)
+                if target_location != -1:
+                    mem.seek(start + target_location)
+                    difference = max(len(curr_pid) - len(new_pid), 0)
+                    mem.write(new_pid + bytes(difference))
+                else:
+                    heap_ranges.pop(0)
+            except IndexError:
+                break
+        mem.close()
+
 
 def error_handler(msg):
     """ prints error message and exits with error code 1 """
     print(msg)
     exit(1)
 
+
 if __name__ == '__main__':
 
-        if len(sys.argv) != 4:
-            error_handler('Usage: {} pid curr_pid new_pid\n'.format(sys.argv[0]))
+    if len(sys.argv) != 4:
+        error_handler('Usage: {} pid curr_pid new_pid\n'.format
+                      (sys.argv[0]))
+    try:
+        pid = int(sys.argv[1])
+    except ValueError:
+        error_handler('{}: first arg must be valid proccess id\n'.format
+                      (sys.argv[0]))
+    curr_pid = bytes(sys.argv[2], 'ascii')
+    new_pid = bytes(sys.argv[3], 'ascii')
 
-        try:
-            pid = int(sys.argv[1])
-        except ValueError:
-            error_handler('{}: first arg must be valid proccess id\n'.format(sys.argv[0]))
-
-        curr_pid = bytes(sys.argv[2], 'ascii')
-        new_pid = bytes(sys.argv[3], 'ascii')
-
-        read_write_heap(pid, curr_pid, new_pid)
-
+    read_write_heap(pid, curr_pid, new_pid)
