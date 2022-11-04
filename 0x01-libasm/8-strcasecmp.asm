c825bb72783f598b2e78b50c15032fad6fc7282a
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-11-04 09:59:01 -0500

    testing 8

---
+++ 0x01-libasm/8-strcasecmp.asm
@@ -0,0 +1,59 @@
+BITS 64
+	global asm_strcasecmp
+	section .text
+
+asm_strcasecmp:
+	push rbp
+	mov rbp, rsp
+	push rdx
+
+while:
+	mov rax, rdi 				; rax = s1
+	mov rdx, rsi				; rdx = s2
+	movzx eax, BYTE [rax]		; rax = *s1
+	movzx edx, BYTE [rdx]
+	cmp al, 0x0
+	jne not_both_null
+	cmp dl, 0x0
+	je _L2
+	not_both_null:
+	cmp al, 65
+	jl case_a_done
+	cmp al, 90
+	jg case_a_done
+	add ax, 32
+	case_a_done:
+	cmp dl, 65
+	jl _L1
+	cmp dl, 90
+	jg _L1
+	add dx, 32
+
+_L1:
+	cmp al, dl
+	jne _L2
+	inc rdi
+	inc rsi
+	jmp while
+
+_L2:
+	cmp al, dl
+	je _isEqual
+	jl _isLess
+	sub al, dl
+	jmp _final
+_isEqual:
+	mov rax, 0x0
+	jmp _final
+_isLess:
+	sub al, dl
+	neg al
+	imul eax, -1
+	jmp _final
+
+_final:
+	pop rdx
+
+	mov rbp, rsp
+	pop rbp
+	ret
