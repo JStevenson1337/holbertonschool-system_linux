a67200332ae3004eaa27fa810c8d38f44a9e62c7
Jeremy Stevenson <jmstevenson918@gmail.com>
2022-11-08 17:58:05 -0600

    fid: task 0

--- 0x00-signals/0-handle_signal.c
+++ 0x00-signals/0-handle_signal.c
@@ -1,16 +1,22 @@
 #include "signals.h"
+
 /**
- *
- *
- *
- *
+ * signal_handler - prints a message when a signal is received
+ * @my_sig: the signal number
+ */
+void signal_handler(int my_sig)
+{
+	printf("Gotcha! [%d]\n", my_sig);
+	fflush(stdout);
+}
+
+/**
+ * handle_signal - sets a handler for the signal SIGINT
+ * Return: 0 on success, -1 on error
  */
 int handle_signal(void)
 {
-
-	if (signal(SIGINT, current_handler) == SIGINT)
+	if (signal(SIGINT, signal_handler) == SIG_ERR)
 		return (-1);
-	printf("Gotcha! [%d]", signal);
-  return (0);
+	return (0);
 }
-
