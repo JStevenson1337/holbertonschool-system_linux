958eae602b57e1a4330a725096f76c54b73c4141
Jeremy Stevenson <jmstevenson918@gmail.com>
2023-01-09 03:27:45 -0600

    try with memcopy

--- 0x00-strace/src/ex0.c
+++ 0x00-strace/src/ex0.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-// extern char **environ;
+extern char **environ;
 /**
  * main - Entry Point
  * argc: argument count
@@ -15,16 +15,32 @@
 */
 int main(int argc, char *argv[])
 {
-
+	int currArg;
 	char *cmd = argv[1];
-	char *args[] = {NULL};
-	char *env[] = {NULL};
+	char *args[] = {};
+	// char **environ};
 
+	if (argc == 1)
+	{
+		printf("No Command passed to %s\n", argv[0]);
+	}
+	if (argc > 2)
+	{
+		printf("Here!\n");
+	}
+	for (currArg = 0; currArg < argc; currArg++)
+	{
+		printf("%s\n", argv[currArg]);
+		// snprintf(args, sizeof(argv), "%s", argv);
+	}
+	// memcpy(&args, argv, sizeof(char **));
+	*args -> argv;
 	execve(
 		cmd,
 		args,
-		env
+		environ
 	);
+	printf(">>> Error = %i\n", errno);
 
 
 	return 0;
