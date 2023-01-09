a3e4993b3090fdd761219c9c51cc289cf8f3cf73
Jeremy Stevenson <jmstevenson918@gmail.com>
2023-01-09 06:45:48 -0600

    using linked list to build command options

--- 0x00-strace/src/ex0.c
+++ 0x00-strace/src/ex0.c
@@ -4,6 +4,61 @@
 #include <string.h>
 #include <unistd.h>
 extern char **environ;
+#define CURRENT_ARG
+
+
+typedef struct node
+{
+	int data;
+	struct node *next;
+} node_t;
+
+node_t *head;
+
+void insert(int x)
+{
+	node_t *temp = malloc(sizeof(node_t));
+	temp -> data = x;
+	temp -> next = NULL;
+
+	if (head != NULL)
+	{
+		temp -> next = head;
+	}
+	head = temp;
+}
+
+void copy(node_t *head, int arr[CURRENT_ARG], int n)
+{
+	node_t *temp = malloc(sizeof(node_t));
+	temp -> data = arr[0];
+	temp -> head = NULL;
+	head = temp;
+
+	int i;
+	for (i = 1; i < n; i++)
+	{
+		node_t *temp2 = malloc(sizeof(node_t));
+		temp -> next = temp2;
+		temp2 -> data = arr[i];
+		temp2 -> = NULL;
+		temp = temp2;
+	}
+}
+
+void printList()
+{
+	node_t *temp = head;
+	printf("List is : ");
+
+	while (temp != NULL)
+	{
+		printf(" %d ", temp -> data);
+		temp = temp -> next;
+	}
+	printf("\n");
+}
+
 /**
  * main - Entry Point
  * argc: argument count
@@ -15,7 +70,7 @@ extern char **environ;
 */
 int main(int argc, char *argv[])
 {
-	int currArg;
+	int i;
 	char *cmd = argv[1];
 	char *args[] = {};
 	// char **environ};
@@ -27,14 +82,32 @@ int main(int argc, char *argv[])
 	if (argc > 2)
 	{
 		printf("Here!\n");
+		*argv = (int *)calloc(CURRENT_ARG, sizeof(char));
+		int i = 0;
+		for (i = 0; i < CURRENT_ARG; i++)
+		{
+			printf("arrays = [%d] ", i);
+			scanf("%s", &argv[i]);
+		}
+		for (i = 0; i < CURRENT_ARG; i++)
+		{
+			printf("array [%s] = %s \n", i, argv[i]);
+
+			copy(head, argv[CURRENT_ARG], CURRENT_ARG);
+			printList();
+
+			getchar();
+
+		}
 	}
-	for (currArg = 0; currArg < argc; currArg++)
-	{
-		printf("%s\n", argv[currArg]);
-		// snprintf(args, sizeof(argv), "%s", argv);
-	}
-	// memcpy(&args, argv, sizeof(char **));
-	*args -> argv;
+	// for (CURRENT_ARG = 0; CURRENT_ARG < argc; CURRENT_ARG++)
+	// {
+	// 	printf("%s\n", argv[CURRENT_ARG]);
+	// 	// snprintf(args, sizeof(argv), "%s", argv);
+	// }
+	// memcpy(args, argv, sizeof(char *));
+
+	
 	execve(
 		cmd,
 		args,
