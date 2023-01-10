c7a1be82bd0940571bad113894fbc04206facf33
Jeremy Stevenson <jmstevenson918@gmail.com>
2023-01-10 09:34:39 -0600

    up to filling the struct

--- 0x00-strace/inc/header.h
+++ 0x00-strace/inc/header.h
@@ -1,14 +1,15 @@
 #ifndef __HEADER_H__
 #define __HEADER_H__
 
-#include <errno.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <sys/ptrace.h>
-
-
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/user.h>
 
 
 
--- 0x00-strace/src/main.c
+++ 0x00-strace/src/main.c
@@ -1,5 +1,6 @@
 #include "../inc/header.h"
 #include "../inc/syscalls.h"
+#include <stdlib.h>
 /**
  * main - Entry Point
  * argc: argument count
@@ -10,52 +11,62 @@ int main(int argc, char *argv[], char **environ) {
 	pid_t pid;
 	syscall_t sysInfo;
 	int mainThread = 1;
+	int stat_loc;
 
 
 
 
-  while (mainThread)
-  {
-	pid = fork();
-
-	printf("%d\t initial Fork\n", pid);
-
-	if (pid == 0)
+	while (mainThread)
 	{
-		printf("%d\t inside Child Condition\n", pid);
+		pid = fork();
 
-		ptrace(PTRACE_TRACEME, pid, NULL, NULL);
+		if (pid == -1)
+		{
+			printf("errno %i\n", errno);
+			return (EXIT_FAILURE);
+		}
+		if ( pid == 0)
+		{
+			ptrace(PTRACE_TRACEME, 0, 0, 0);
 
-		execve(argv[1], &argv[1], NULL);
-		/* Execve Faild */
-		printf(">>> Error = %i\n", errno);
-		return (EXIT_FAILURE);
+			/* Execute the process */
+			argv[argc] = 0;
+
+			execve(argv[1], &argv[1], NULL);
+			/*TODO: The success of execve will cause a
+			SIGTRAP to be sent to this child process. */
+			
+			
+
+		}
+		/* In parent */
+
+		/* Wait for execve to finish*/
+
+		pid_t wait(int *stat_loc);
+		/* Start to trace system calls */
+
+		ptrace(PTRACE_SYSCALL, pid, 0, 0);
+		/* TODO: Wait until the ennty to a sys call */
+		sysInfo[0] = {nr};
+		struct user_regs_struct u_in;
+		/* TODO: Fill Struct */
+
+		ptrace(PTRACE_GETREGS, pid, 0, &u_in);
+		
+
+		syscall = u_in.orig_eax;
+		/* TODO: Restart Child if stopped */
+		
+		if (syscall == __NR_open)
+		{
+			printf("%s", syscall_names[syscall-1]);
+ 			printf("%08lx ", u_in.ebx);
+			printf("%08lx ", u_in.ecx);
+			printf("%08lx\n ", u_in.edx);
+		}
+		mainThread--;
 	}
-
-	if (pid > 0)
-	{
-
-		// int wait(NULL);
-		// orig_eax = ptrace(PTRACE_GETREGS, )
-		/* TODO: Inside Parent Process */
-		// pid_t pPid = pid;
-		printf("%d\t inside Parent Condition\n", pid);
-	}
-	else
-	{
-		/* Fork Failed Exit */
-		printf("Error = %i\n", errno);
-		return (EXIT_FAILURE);
-	}
-
-	if (argc < 2)
-	{
-		printf("Error -> %i\n", errno);
-		return (EXIT_FAILURE);
-	}
-
-	mainThread--;
-	}
-
+	
 	return 0;
 }
