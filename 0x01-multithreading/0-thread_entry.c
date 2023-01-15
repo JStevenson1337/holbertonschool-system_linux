9fa0415478f1326c26b1696c07a4a0c2bc5eb9f2
jstevenson1337 <jmstevenson918@gmail.com>
2023-01-14 21:54:59 -0600

    checking 0

--- 0x01-multithreading/0-thread_entry.c
+++ 0x01-multithreading/0-thread_entry.c
@@ -1,18 +1,19 @@
 #include "multithreading.h"
 #include <stdlib.h>
-
-
-
+/**
+  * thread_entry - entry point for a new thread
+  * 
+  * @arg: pointer to the address of a string
+  */
 void *thread_entry(void *arg)
 {
-	// char *str;
-	// int i = 0;
-
-	// str = (char *)arg;
 	if (arg == NULL)
 	{
 		pthread_exit(&arg);
 	}
+
+	printf("%s\n", (char *)arg);
+	pthread_exit(&arg);
 	return (0);
 }
 
