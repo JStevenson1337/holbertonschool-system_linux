8bd2447d59a8cf1e7318ef5f724af609b17f4a7a
jstevenson1337 <jmstevenson918@gmail.com>
2023-01-17 15:10:38 -0600

    combined functions

--- 0x01-multithreading/10-blur_portion.c
+++ 0x01-multithreading/10-blur_portion.c
@@ -1,44 +1,5 @@
 #include "multithreading.h"
 #include <stddef.h>
-
-img_t *iMageMap(blur_portion_t const *portion, size_t *surfaceArea, pixel_t *resolution[])
-{
-	size_t *width, *height;
-
-	width = (size_t *)(portion->w);
-	height = (size_t *)(portion->h);
-	*surfaceArea = *width * *height;
-
-
-
-
-
-	
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /**
  * blur_portion - apply gaussian blur to an image
  *
@@ -47,12 +8,19 @@ img_t *iMageMap(blur_portion_t const *portion, size_t *surfaceArea, pixel_t *res
  */
 void blur_portion(blur_portion_t const *portion)
 {
+	size_t *width, *height, *SA;
+
+	width = (size_t *)(portion->w);
+	height = (size_t *)(portion->h);
+	
+
+
 	if (!portion || !portion->img || !portion->kernel)
 	{
 		printf("Error %d\n", errno);
 		pthread_exit(0);
 	}
 
-	
+	*SA = *width * *height;
 
 }
