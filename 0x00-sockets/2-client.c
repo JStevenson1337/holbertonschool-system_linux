1ebc11add63c410b9b909d47b2e3c7b996dd3e8d
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2023-02-27 19:22:55 -0600

    checking 2-client

--- 0x00-sockets/2-client.c
+++ 0x00-sockets/2-client.c
@@ -4,68 +4,55 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <errno.h>
+#include <stdlib.h>
+
 
 int main(int argc, char **argv)
 {
-	ushort port;
-	char *host;
-	struct sockaddr_in server_getaddrinfo,
-						client_getaddrinfo;
-	unsigned int client_length;
-	int connector, connection_string;
-	struct in_addr
-	{
-		unsigned long s_addr;
-	}
+	struct addrinfo hints;
+    struct addrinfo *result, *rp;
+    struct sockaddr_in *sa;
+    int s;
 
-	if (argc < 2)
-	{
-		printf("Usage: %s <host> <port>\n", argv[0]);
-		return (EXIT_FAILURE);
-	}
-	*host = argv[1];
-	port = atoi(argv[2]);
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
-	// create_socket(host, port)
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
 
-	client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (!client_socket)
-	{
-		perror("socket");
-		return (EXIT_FAILURE);
-	}
+    s = atoi(argv[1]);
+    if (s <= 0 || s > 65535) {
+        fprintf(stderr, "Invalid port: %s\n", argv[1]);
+        exit(1);
+    }
 
-	echo_server_address.sin_family = AF_INET;
-	echo_server_address.sin_addr.s_addr = 
-		inet_addr(echo_server_address.sin_addr.s_addr);
-	echo_server_address.sin_port = htons(echoServPort);
+    sa = calloc(1, sizeof(struct sockaddr_in));
+	if (!sa) {
+        perror("calloc");
+        exit(1);
+    }
+	sa->sin_family = AF_INET;
+	sa->sin_port = htons(s);
+	sa->sin_addr.s_addr = htonl(INADDR_ANY);
 
-	connect(client_socket,
-			(struct sockaddr *)&echo_server_address,
-			sizeof(echo_server_address));
-
-	if (connect != 0)
-	{
-		perror("connect");
-		return (EXIT_FAILURE);
-	}
-
-	for (;;)
-	{
-		echo_string_length = strlen(echo_string);
-
-		send(client_socket, echoString,
-			echo_string_length, 0);
-		if (send == -1)
-		{
-			perror("send");
-			perror("errno(send)");
-		}
-	}
-
-
-
-
-
-
+	result = NULL;
+	errno = 0;
+	s = getaddrinfo(argv[0], argv[1], &hints, &result);
+	if (s!= 0) {
+        perror("getaddrinfo");
+        exit(1);
+    }
+	rp = result;
+	while (rp) {
+        printf("%s:%s\n", rp->ai_canonname, rp->ai_addr->sa_data);
+        rp = rp->ai_next;
+    }
+	freeaddrinfo(result);
+	exit(0);
+	return 0;
 }
